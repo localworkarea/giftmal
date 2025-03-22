74882045ba8f275fc406db0d5a02fd17e89f3745
localworkarea <localworkarea@gmail.com>
2025-03-22 22:22:02 +0200

    edits

--- src/js/files/functions.js
+++ src/js/files/functions.js
@@ -129,6 +129,74 @@ export let _slideToggle = (target, duration = 500, showmore = 0, maxHeight = 0) 
 			return _slideUp(target, duration, showmore);
 	}
 };
+// export let _slideUp = (target, duration = 500, showmore = 0) => {
+// 	if (!target.classList.contains('_slide')) {
+// 		target.classList.add('_slide');
+// 		target.style.transitionProperty = 'height, margin, padding';
+// 		target.style.transitionDuration = duration + 'ms';
+// 		target.style.height = `${target.offsetHeight}px`;
+// 		target.offsetHeight;
+// 		target.style.overflow = 'hidden';
+// 		target.style.height = showmore ? `${showmore}px` : `0px`;
+// 		target.style.paddingTop = 0;
+// 		target.style.paddingBottom = 0;
+// 		target.style.marginTop = 0;
+// 		target.style.marginBottom = 0;
+// 		window.setTimeout(() => {
+// 			target.hidden = !showmore ? true : false;
+// 			!showmore ? target.style.removeProperty('height') : null;
+// 			target.style.removeProperty('padding-top');
+// 			target.style.removeProperty('padding-bottom');
+// 			target.style.removeProperty('margin-top');
+// 			target.style.removeProperty('margin-bottom');
+// 			!showmore ? target.style.removeProperty('overflow') : null;
+// 			target.style.removeProperty('transition-duration');
+// 			target.style.removeProperty('transition-property');
+// 			target.classList.remove('_slide');
+// 			// Створюємо подію 
+// 			document.dispatchEvent(new CustomEvent("slideUpDone", {
+// 				detail: {
+// 					target: target
+// 				}
+// 			}));
+// 		}, duration);
+// 	}
+// }
+// export let _slideDown = (target, duration = 500, showmore = 0) => {
+// 	if (!target.classList.contains('_slide')) {
+// 		target.classList.add('_slide');
+// 		target.hidden = target.hidden ? false : null;
+// 		showmore ? target.style.removeProperty('height') : null;
+// 		let height = target.offsetHeight;
+// 		target.style.overflow = 'hidden';
+// 		target.style.height = showmore ? `${showmore}px` : `0px`;
+// 		target.style.paddingTop = 0;
+// 		target.style.paddingBottom = 0;
+// 		target.style.marginTop = 0;
+// 		target.style.marginBottom = 0;
+// 		target.offsetHeight;
+// 		target.style.transitionProperty = "height, margin, padding";
+// 		target.style.transitionDuration = duration + 'ms';
+// 		target.style.height = height + 'px';
+// 		target.style.removeProperty('padding-top');
+// 		target.style.removeProperty('padding-bottom');
+// 		target.style.removeProperty('margin-top');
+// 		target.style.removeProperty('margin-bottom');
+// 		window.setTimeout(() => {
+// 			target.style.removeProperty('height');
+// 			target.style.removeProperty('overflow');
+// 			target.style.removeProperty('transition-duration');
+// 			target.style.removeProperty('transition-property');
+// 			target.classList.remove('_slide');
+// 			// Створюємо подію
+// 			document.dispatchEvent(new CustomEvent("slideDownDone", {
+// 				detail: {
+// 					target: target
+// 				}
+// 			}));
+// 		}, duration);
+// 	}
+// }
 // export let _slideToggle = (target, duration = 500) => {
 // 	if (target.hidden) {
 // 		return _slideDown(target, duration);
@@ -136,6 +204,7 @@ export let _slideToggle = (target, duration = 500, showmore = 0, maxHeight = 0) 
 // 		return _slideUp(target, duration);
 // 	}
 // }
+
 
 // Допоміжні модулі блокування прокручування та стрибка ====================================================================================================================================================================================================================================================================================
 export let bodyLockStatus = true
